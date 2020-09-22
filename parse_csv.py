f6099d86a3600be192458d24a22d35e16ec9085c
Nick Young <nick.young@auckland.ac.nz>
2020-09-22 17:54:22 +1200

    project reorganisation, switch to TALB

--- parse_csv.py
+++ parse_csv.py
@@ -5,40 +5,78 @@ import sys
 import re
 import json
 
-files = sys.argv[1:]
-o = {}
+with open("data/TALB_2018.geojson") as f:
+  geojson = json.load(f)
 
-for fn in files:
-  with open(fn) as f:
-    reader = csv.DictReader(f)
-    for row in reader:
+def find_talb(name):
+  for i,f in enumerate(geojson["features"]):
+    if f["properties"]["TALB2018_1"] == name:
+      return i
+  print(f + " not found")
+  exit(1)
+
+for i, f in enumerate(geojson["features"]):
+  del geojson["features"][i]["properties"]["smoking"]
+
+with open("data/2018_census/2018-census-place-summaries-health-table2-2018-csv.csv") as f:
+  reader = csv.DictReader(f)
+  for row in reader:
 #"Year","Area_type","Area_code","Area_description","Cigarette_smoking_behaviour_code","Cigarette_smoking_behaviour_description","Sex_code","Sex_description","Age_group_life_cycle_groups_code","Age_group_life_cycle_groups_description","Maori_ethnic_group_indicator_summary_code","Maori_ethnic_group_indicator_summary_description","Census_usually_resident_population_count_aged_15_years_and_over","Cigarette_smoking_behaviour_by_Maori_ethnic_group_indicator_summary_percent","Cigarette_smoking_behaviour_by_sex_percent","Cigarette_smoking_behaviour_percent"
-      if row["Area_type"] == "Statistical Area 2":
-        code = row["Area_code"]
-        year = row["Year"]
-        smoker = row["Cigarette_smoking_behaviour_description"]
-        age_group = row["Age_group_life_cycle_groups_description"]
-        sex = row["Sex_description"]
-        maori = row["Maori_ethnic_group_indicator_summary_description"]
-        if maori != "Total":
-          continue
-        count = row["Census_usually_resident_population_count_aged_15_years_and_over"]
-        if count == "C":
-          count = None
-        else:
-          count = int(count)
-        if code not in o:
-          o[code] = {
-            "Male": {},
-            "Female": {},
-            "Total": 0
-          }
-        if smoker == "Total stated" and sex == "Total" and age_group == "Total" and count:
-          o[code]["Total"] += count
-        if smoker in ["Total", "Total stated", "Not elsewhere included"] or age_group in ["Under 15 years", "Total"] or sex == "Total":
-          continue
-        if smoker not in o[code][sex]:
-          o[code][sex][smoker] = {}
-        o[code][sex][smoker][age_group] = count
+    if row["Area_type"] == "Territorial Authority Local Board":
+      area = row["Area_description"]
+      if area == "Total":
+        continue
+      year = row["Year"]
+      smoker = row["Cigarette_smoking_behaviour_description"]
+      age_group = row["Age_group_life_cycle_groups_description"]
+      sex = row["Sex_description"]
+      maori = row["Maori_ethnic_group_indicator_summary_description"]
+      count = row["Census_usually_resident_population_count_aged_15_years_and_over"]
+      if count == "C":
+        continue
+      else:
+        count = int(count)
+      i = find_talb(area)
+      if "smoking" not in geojson["features"][i]["properties"]:
+        geojson["features"][i]["properties"]["smoking"] = {
+          "maori_male_smoker": 0,
+          "maori_male_non-smoker": 0,
+          "maori_female_smoker": 0,
+          "maori_female_non-smoker": 0,
+          "non-maori_male_smoker": 0,
+          "non-maori_male_non-smoker": 0,
+          "non-maori_female_smoker": 0,
+          "non-maori_female_non-smoker": 0,
+          "total_15+": 0
+        }
+      if age_group != "Total":
+        continue
+      if smoker in ["Regular smoker", "Ex-smoker"]:
+        if maori == "Maori":
+          if sex == "Male":
+            geojson["features"][i]["properties"]["smoking"]["maori_male_smoker"] += count
+          elif sex == "Female":
+            geojson["features"][i]["properties"]["smoking"]["maori_female_smoker"] += count
+        elif maori == "No 'Maori' response given":
+          if sex == "Male":
+            geojson["features"][i]["properties"]["smoking"]["non-maori_male_smoker"] += count
+          elif sex == "Female":
+            geojson["features"][i]["properties"]["smoking"]["non-maori_female_smoker"] += count
+      elif smoker == "Never smoked regularly":
+        if maori == "Maori":
+          if sex == "Male":
+            geojson["features"][i]["properties"]["smoking"]["maori_male_non-smoker"] += count
+          elif sex == "Female":
+            geojson["features"][i]["properties"]["smoking"]["maori_female_non-smoker"] += count
+        elif maori == "No 'Maori' response given":
+          if sex == "Male":
+            geojson["features"][i]["properties"]["smoking"]["non-maori_male_non-smoker"] += count
+          elif sex == "Female":
+            geojson["features"][i]["properties"]["smoking"]["non-maori_female_non-smoker"] += count
+      if smoker == "Total" and maori == "Total" and sex == "Total":
+        geojson["features"][i]["properties"]["smoking"]["total_15+"] += count
 
-print(json.dumps(o, sort_keys=True, indent=2))
+
+
+with open("data/TALB_2018.geojson", "w") as f:
+  json.dump(geojson, f)
--- parse_dan_excel.py
+++ parse_dan_excel.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
 import pandas as pd
 import sys
+import json
+
+with open("data/TALB_2018.geojson") as f:
+  geojson = json.load(f)
+
+def find_talb(name):
+  for i,f in enumerate(geojson["features"]):
+    if f["properties"]["TALB2018_1"] == name:
+      return i
+  print(f + " not found")
+  exit(1)
 
 talb = pd.read_excel("misc/cancer_FOROUTPUT_checked.xlsx", sheet_name="TALB", skiprows=11, usecols="B:H,L:Q",
     names = ["TALB", "maori_prostate_cancer", "maori_breast_cancer", "maori_lung_cancer", "maori_male_all_cancer_40-69", "maori_female_all_cancer_45-69", "maori_all_cancer_18+",
         "non-maori_prostate_cancer", "non-maori_breast_cancer", "non-maori_lung_cancer", "non-maori_male_all_cancer_40-69", "non-maori_female_all_cancer_45-69", "non-maori_all_cancer_18+"]
 )
 
-talb.to_csv("TALB_cancer.csv", index=False)
+for i, row in talb.iterrows():
+    print(row["TALB"])
+    if pd.isna(row["TALB"]):
+        continue
+    i = find_talb(row["TALB"])
+    row = dict(row)
+    del row["TALB"]
+    geojson["features"][i]["properties"]["cancer"] = row
 
-ward = pd.read_excel("misc/cancer_FOROUTPUT_checked.xlsx", sheet_name="WARD", skiprows=13, usecols="B:H,L:Q",
-    names = ["WARD", "maori_prostate_cancer", "maori_breast_cancer", "maori_lung_cancer", "maori_male_all_cancer_40-69", "maori_female_all_cancer_45-69", "maori_all_cancer_18+",
-        "non-maori_prostate_cancer", "non-maori_breast_cancer", "non-maori_lung_cancer", "non-maori_male_all_cancer_40-69", "non-maori_female_all_cancer_45-69", "non-maori_all_cancer_18+"]
-)
-
-ward.to_csv("WARD_cancer.csv", index=False)
+with open("data/TALB_2018.geojson", "w") as f:
+  json.dump(geojson, f)
