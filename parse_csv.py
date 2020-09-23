ed599b3849e7c6e8bcccc468f34d9d1f36061bea
Nick Young <nick.young@auckland.ac.nz>
2020-09-23 12:43:15 +1200

    add age info

--- parse_csv.py
+++ parse_csv.py
@@ -76,6 +76,37 @@ with open("data/2018_census/2018-census-place-summaries-health-table2-2018-csv.c
       if smoker == "Total" and maori == "Total" and sex == "Total":
         geojson["features"][i]["properties"]["smoking"]["total_15+"] += count
 
+with open("data/2018_census/2018-census-place-summaries-popdwell-table3-2018-csv.csv") as f:
+  reader = csv.DictReader(f)
+  for row in reader:
+    #"Year","Area_type","Area_code","Area_description","Age_group_5_year_groups_to_85_years_and_over_code","Age_group_5_year_groups_to_85_years_and_over_description","Sex_code","Sex_description","Maori_ethnic_group
+#_indicator_summary_code","Maori_ethnic_group_indicator_summary_description","Census_usually_resident_population_count","Age_group_5_year_groups_to_85_years_and_over_by_Maori_ethnic_group_indicator_summary_perc
+#ent","Age_group_5_year_groups_to_85_years_and_over_by_sex_percent"
+      if row["Area_type"] == "Territorial Authority Local Board":
+        area = row["Area_description"]
+        if area == "Total":
+          continue
+        i = find_talb(area)
+        year = row["Year"]
+        age_group = row["Age_group_5_year_groups_to_85_years_and_over_description"]
+        sex = row["Sex_description"]
+        if sex != "Total":
+          continue
+        maori = row["Maori_ethnic_group_indicator_summary_description"]
+        if maori == "No 'Maori' response given":
+          maori = "Non-maori"
+        count = row["Census_usually_resident_population_count"]
+        if count == "C":
+          count = None
+        else:
+          count = int(count)
+        if "age" not in geojson["features"][i]["properties"]:
+          geojson["features"][i]["properties"]["age"] = {
+            "Maori": {},
+            "Non-maori": {},
+            "Total": {}
+          }
+        geojson["features"][i]["properties"]["age"][maori][age_group] = count
 
 
 with open("data/TALB_2018.geojson", "w") as f:
