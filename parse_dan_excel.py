60437ce1a660b41bed59be2a6e3254ffd9b8e5b6
Nick <nick.young@auckland.ac.nz>
2020-10-28 17:01:56 +1300

    updates for new temporal data

--- parse_dan_excel.py
+++ parse_dan_excel.py
@@ -6,26 +6,52 @@ import json
 with open("data/TALB_2018.geojson") as f:
   geojson = json.load(f)
 
+for i in range(len(geojson["features"])):
+  geojson["features"][i]["properties"]["cancer"] = {}
+
 def find_talb(name):
+  if pd.isna(name):
+    return
   for i,f in enumerate(geojson["features"]):
-    if f["properties"]["TALB2018_1"] == name:
+    if f["properties"]["TALB2018_1"].startswith(name.replace(".", "")):
       return i
-  print(f + " not found")
-  exit(1)
+  print(name + " not found")
 
-talb = pd.read_excel("misc/cancer_FOROUTPUT_checked.xlsx", sheet_name="TALB", skiprows=11, usecols="B:H,L:Q",
-    names = ["TALB", "maori_prostate_cancer", "maori_breast_cancer", "maori_lung_cancer", "maori_male_all_cancer_40-69", "maori_female_all_cancer_45-69", "maori_all_cancer_18+",
-        "non-maori_prostate_cancer", "non-maori_breast_cancer", "non-maori_lung_cancer", "non-maori_male_all_cancer_40-69", "non-maori_female_all_cancer_45-69", "non-maori_all_cancer_18+"]
-)
+year_bands = ["2010-2012", "2013-2015", "2016-2018"]
+cancer_types = ["breast", "prostate", "lung"]
+maori = ["maori", "non-maori", "total"]
+keys = ["TALB"]
+for c in cancer_types:
+  for m in maori:
+    for y in year_bands:
+      keys.append(c + m + y)
 
-for i, row in talb.iterrows():
+numerator = pd.read_excel("misc/annual_counts_OUTPUT - Checked.xlsx", sheet_name="TALB", skiprows=10, nrows=88, names = keys)
+print(numerator)
+cancer_types = ["females 45-69 all cancer", "males 40-69 all cancer", "total 18+ all cancer"]
+keys = ["TALB"]
+for c in cancer_types:
+  for m in maori:
+    for y in year_bands:
+      keys.append(c + m + y)
+denominator = pd.read_excel("misc/annual_counts_OUTPUT - Checked.xlsx", sheet_name="TALB", skiprows=99, nrows=88, names = keys)
+print(denominator)
+
+for i, row in numerator.iterrows():
     print(row["TALB"])
-    if pd.isna(row["TALB"]):
-        continue
     i = find_talb(row["TALB"])
-    row = dict(row)
-    del row["TALB"]
-    geojson["features"][i]["properties"]["cancer"] = row
+    if i:
+      row = dict(row)
+      del row["TALB"]
+      geojson["features"][i]["properties"]["cancer"].update(row)
+
+for i, row in denominator.iterrows():
+    print(row["TALB"])
+    i = find_talb(row["TALB"])
+    if i:
+      row = dict(row)
+      del row["TALB"]
+      geojson["features"][i]["properties"]["cancer"].update(row)
 
 with open("data/TALB_2018.geojson", "w") as f:
   json.dump(geojson, f)
